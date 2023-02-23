e32241af6bddd43d2238e1608aa15370ad33113d
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2023-02-22 21:47:12 -0300

    agregando Tailwind CSS

--- src/app/headerNav.js
+++ src/app/headerNav.js
@@ -1,9 +1,12 @@
-
-
 export const HeaderNav = () => {
-    return(
-        <div>
-            <h1>Este es un Header</h1>
-        </div>
-    )
-}
+  return (
+    <div class="flex ...">
+      <div class="flex-1 ...">01</div>
+      <div class="contents">
+        <div class="flex-1 ...">02</div>
+        <div class="flex-1 ...">03</div>
+      </div>
+      <div class="flex-1 ...">04</div>
+    </div>
+  );
+};
