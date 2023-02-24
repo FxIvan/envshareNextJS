e35992450ae83f712c81d879c8206403f177a0fc
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2023-02-24 12:17:15 -0300

    creado componente InputTextArea

---
+++ src/app/component/inputTextArea/inputTextArea.js
@@ -0,0 +1,7 @@
+export default function InputTextArea(){
+    return(
+        <div>
+            <h2 className="text-white">Input</h2>
+        </div>
+    )
+}
--- src/app/share/page.js
+++ src/app/share/page.js
@@ -1,8 +1,14 @@
+import InputTextArea from "../component/inputTextArea/inputTextArea";
+
 export default function Share(){
     return(
         <div>
-            
+            <div>
             <h1 className="py-4 text-5xl font-bold text-center text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/60 to-white">Encrypt and Share</h1>
+            </div>
+            <div>
+                <InputTextArea/>
+            </div>
         </div>
     )
 }
